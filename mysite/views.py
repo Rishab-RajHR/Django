7309722a731fae3e673907910a64e84efc1b4705
Rishab Raj <rr0948555@gmail.com>
2026-02-03 14:37:38 +0530

    Printing the data on UI

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
 
 def home(request):
+    employees = Employee.objects.all()
+    print(employees)
     return render(request, 'home.html')
