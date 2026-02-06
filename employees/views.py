2a1755336648fa454fe38934941bfa55b019bdf5
Rishab Raj <rr0948555@gmail.com>
2026-02-06 14:44:30 +0530

    Bootstrap

--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-
 from employees.models import Employee
 
-# Create your views here.
-
-def employee_detail(require, pk):
+def employee_detail(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
-    return HttpResponse(employee)
+    context = {
+        'employee' : employee
+    }
+    return render(request, 'employee_detail.html')
